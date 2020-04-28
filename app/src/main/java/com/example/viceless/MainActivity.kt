ca10648c39d60699cd9b8e6581cebd22a41cafd2
Ewald <ewald.dobre@gmail.com>
2020-04-29 00:32:51 +0300

    improved smoking intro slides;

--- app/src/main/java/com/example/viceless/MainActivity.kt
+++ app/src/main/java/com/example/viceless/MainActivity.kt
@@ -1,7 +1,17 @@
 package com.example.viceless
 
+import android.content.Intent
+import android.media.Image
+import android.os.Build
 import android.os.Bundle
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
+import androidx.viewpager2.widget.ViewPager2
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -24,5 +34,67 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
         introSliderViewPager.adapter = introSliderAdapter
+
+        setupIndicators()
+        setCurrentIndicator(0)
+        introSliderViewPager.registerOnPageChangeCallback(object :
+            ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    setCurrentIndicator(position)
+                }
+            }
+        )
+
+        buttonNext.setOnClickListener {
+            if (introSliderViewPager.currentItem + 1 < introSliderAdapter.itemCount) {
+                introSliderViewPager.currentItem += 1
+            } else {
+                Intent(applicationContext, SmokingHomeActivity::class.java).also {
+                    startActivity(it)
+                }
+            }
+        }
+        skip_button.setOnClickListener {
+            Intent(applicationContext, SmokingHomeActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+    }
+
+    private fun setupIndicators() {
+        val indicators = arrayOfNulls<ImageView>(introSliderAdapter.itemCount)
+        val layoutParams: LinearLayout.LayoutParams =
+            LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+
+        layoutParams.setMargins(8, 0, 8, 0)
+        for (i in indicators.indices) {
+            indicators[i] = ImageView(applicationContext)
+            indicators[i].apply {
+                this?.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_inactive
+                    )
+                )
+                this?.layoutParams = layoutParams
+            }
+            indicatorsContainer.addView(indicators[i])
+        }
+    }
+
+    private fun setCurrentIndicator(index: Int) {
+        val childCount = indicatorsContainer.childCount
+
+        for (i in 0 until childCount) {
+            val imageView = indicatorsContainer[i] as ImageView
+            imageView.setImageDrawable(
+                ContextCompat.getDrawable(
+                    applicationContext,
+                    if (i == index) R.drawable.indicator_active else R.drawable.indicator_inactive
+                )
+            )
+        }
     }
 }
+
---
+++ app/src/main/java/com/example/viceless/SmokingHomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.viceless
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SmokingHomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_smoking_home)
+    }
+}
